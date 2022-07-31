624f75d1c7c1bf0efc2ee18955c646cb03d1cf38
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2022-07-31 14:53:21 +0300

    ed

---
+++ ohrms_core/etmam_font/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "ETMAM FONT", 
+    "summary": "ETMAM FONT",
+    "version": "13.0", 
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "template/assets.xml",
+    ],
+}
