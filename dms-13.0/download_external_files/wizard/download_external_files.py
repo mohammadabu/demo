727224d64a6986e47940a0a04475cc9464ab84f1
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2022-11-07 13:56:48 +0300

    Update download_external_files.py

--- dms-13.0/download_external_files/wizard/download_external_files.py
+++ dms-13.0/download_external_files/wizard/download_external_files.py
@@ -11,18 +11,19 @@ class DownloadExternalFiles(models.TransientModel):
 
     # stages = fields.Many2many('project.project.stages')
     def download_external_files(self):
-
+        _logger.info("download_external_files")
         url = 'https://my.techsfactory.com/web/image/website/1/logo/TechsFactory?unique=b499f19'
         response = requests.get(url)
-        json_data = response.json()
+        response.raise_for_status()
+        if response.status_code != 204:
+            json_data = response.json()
+            _logger.info(json_data)
+        else:
+            _logger.info(response)
         # loaded_json = json.dumps(json_data)
         # book = json_data['items'][0]
         # image_url = book["volumeInfo"]['imageLinks']['smallThumbnail'] // here contains image url such as thumbnail
         # img = base64.b64encode(requests.get(image_url).content)
         # self.photo = img
-
-
-        _logger.info("download_external_files")
-        _logger.info(json_data)
         
 
