3a6c79b6ea232261fe7b3abfd4d218b670f09640
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2022-11-07 13:51:36 +0300

    Update download_external_files.py

--- dms-13.0/download_external_files/wizard/download_external_files.py
+++ dms-13.0/download_external_files/wizard/download_external_files.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+import base64
+import requests
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -9,6 +11,18 @@ class DownloadExternalFiles(models.TransientModel):
 
     # stages = fields.Many2many('project.project.stages')
     def download_external_files(self):
+
+        url = 'https://my.techsfactory.com/web/image/website/1/logo/TechsFactory?unique=b499f19'
+        response = requests.get(url)
+        json_data = response.json()
+        loaded_json = json.dumps(json_data)
+        # book = json_data['items'][0]
+        # image_url = book["volumeInfo"]['imageLinks']['smallThumbnail'] // here contains image url such as thumbnail
+        # img = base64.b64encode(requests.get(image_url).content)
+        # self.photo = img
+
+
         _logger.info("download_external_files")
+        _logger.info(json_data)
         
 
