965466c7e8d534aa2ef22404b768c10609b9941c
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2023-01-15 14:45:11 +0300

    ed

---
+++ theme/custom_hide_modules/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Custom Hide Modules",
+    'author': "Techs Factory",
+    'website': "https://www.techsfactory.com",
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/groups.xml',
+        'security/security.xml'
+    ],
+    'demo': [
+
+    ],
+}
